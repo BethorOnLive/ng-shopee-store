85b69f220ce4968bc9dc723f471ec1256ad6dfaf
Jesús <bethor18@gmail.com>
2022-12-13 11:17:43 -0600

    git inicial repo deploy

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { NgbModule, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
+import { SearchHeaderNavComponent } from './search-header-nav/search-header-nav.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { StoreServiceService } from './main-content/store-service.service';
+import { SharedServiceService } from './shared-service.service'
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    RegisterComponent,
+    SearchHeaderNavComponent,
+    MainContentComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    NgbDatepickerModule,
+    ReactiveFormsModule,
+    NgSelectModule,
+    FormsModule,
+    SweetAlert2Module.forRoot(),
+    HttpClientModule
+  ],
+  providers: [StoreServiceService, SharedServiceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/main-content/store-service.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreServiceService {
+
+
+  constructor(private _httpClient: HttpClient) { }
+
+  private url = 'https://fakestoreapi.com/products?limit=9';
+  //Se limitaron los resultados a 9 elementos hasta que se implemente un paginado 
+
+  getProducts(){
+    return this._httpClient.get<any>(this.url);
+  }
+}
